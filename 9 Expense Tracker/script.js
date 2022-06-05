dc09c278b73ab04b474cda8d31751a7d11594f38
Ionut Ivanciu <ionutziv7@protonmail.com>
2022-06-05 17:53:36 +0300

    Add & delete transactions

--- 9 Expense Tracker/script.js
+++ 9 Expense Tracker/script.js
@@ -13,10 +13,39 @@ const dummyTransactions = [
   { id: 4, text: "Camera", amount: 150 },
 ];
 
-let transations = dummyTransactions;
+// Add transaction
+function addTransaction(e) {
+  e.preventDefault();
+
+  if (text.value.trim() === "" || amount.value.trim() === "") {
+    alert("Please add a text and amount");
+  } else {
+    const transaction = {
+      id: generateID(),
+      text: text.value,
+      amount: +amount.value,
+    };
+
+    transactions.push(transaction);
+
+    addTransactionDOM(transaction);
+
+    updateValues();
+
+    text.value = "";
+    amount.value = "";
+  }
+}
+
+// Generate random ID
+function generateID() {
+  return Math.floor(Math.random() * 100000000);
+}
+
+let transactions = dummyTransactions;
 
 // Add transactions to DOM list
-function addTransactionsDOM(transaction) {
+function addTransactionDOM(transaction) {
   // Get sign
   const sign = transaction.amount < 0 ? "-" : "+";
 
@@ -26,8 +55,11 @@ function addTransactionsDOM(transaction) {
   item.classList.add(transaction.amount < 0 ? "minus" : "plus");
 
   item.innerHTML = `
-    ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span>
-    <button class="delete-btn">x</button>
+    ${transaction.text} <span>${sign}${Math.abs(
+    transaction.amount
+  )}</span> <button class="delete-btn" onclick="removeTransaction(${
+    transaction.id
+  })">x</button>
   `;
 
   list.appendChild(item);
@@ -35,7 +67,7 @@ function addTransactionsDOM(transaction) {
 
 // Update the balance, income and expense
 function updateValues() {
-  const amounts = transations.map((transaction) => transaction.amount);
+  const amounts = transactions.map((transaction) => transaction.amount);
 
   const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
 
@@ -54,12 +86,21 @@ function updateValues() {
   money_minus.innerText = `$${expense}`;
 }
 
+// Remove transaction by ID
+function removeTransaction(id) {
+  transactions = transactions.filter((transaction) => transaction.id !== id);
+
+  init();
+}
+
 // Init app
 function init() {
   list.innerHTML = "";
 
-  transations.forEach(addTransactionsDOM);
+  transactions.forEach(addTransactionDOM);
   updateValues();
 }
 
 init();
+
+form.addEventListener("submit", addTransaction);
