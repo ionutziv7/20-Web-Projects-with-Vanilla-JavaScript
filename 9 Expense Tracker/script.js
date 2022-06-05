7850009b837d3c827589dda508b0465e4d304be2
Ionut Ivanciu <ionutziv7@protonmail.com>
2022-06-05 18:06:30 +0300

    Persist to local storage

--- 9 Expense Tracker/script.js
+++ 9 Expense Tracker/script.js
@@ -6,12 +6,12 @@ const form = document.getElementById("form");
 const text = document.getElementById("text");
 const amount = document.getElementById("amount");
 
-const dummyTransactions = [
-  { id: 1, text: "Flower", amount: -20 },
-  { id: 2, text: "Salary", amount: 300 },
-  { id: 3, text: "Book", amount: -10 },
-  { id: 4, text: "Camera", amount: 150 },
-];
+const localStorageTransactions = JSON.parse(
+  localStorage.getItem("transactions")
+);
+
+let transactions =
+  localStorage.getItem("transactions") !== null ? localStorageTransactions : [];
 
 // Add transaction
 function addTransaction(e) {
@@ -32,6 +32,8 @@ function addTransaction(e) {
 
     updateValues();
 
+    updateLocalStorage();
+
     text.value = "";
     amount.value = "";
   }
@@ -41,8 +43,6 @@ function addTransaction(e) {
 function generateID() {
   return Math.floor(Math.random() * 100000000);
 }
-
-let transactions = dummyTransactions;
 
 // Add transactions to DOM list
 function addTransactionDOM(transaction) {
@@ -90,7 +90,13 @@ function updateValues() {
 function removeTransaction(id) {
   transactions = transactions.filter((transaction) => transaction.id !== id);
 
+  updateLocalStorage();
   init();
+}
+
+// Update local storage transactions
+function updateLocalStorage() {
+  localStorage.setItem("transactions", JSON.stringify(transactions));
 }
 
 // Init app
