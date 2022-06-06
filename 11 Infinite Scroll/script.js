a32466d4bec32fbb29efd7b12aefb0f703978e8a
Ionut Ivanciu <ionutziv7@protonmail.com>
2022-06-06 17:12:22 +0300

    Filter fetched posts

--- 11 Infinite Scroll/script.js
+++ 11 Infinite Scroll/script.js
@@ -1,14 +1,14 @@
-const postContainer = document.getElementById("post-container");
+const postsContainer = document.getElementById("posts-container");
 const loading = document.querySelector(".loader");
 const filter = document.getElementById("filter");
 
-let limit = 3;
+let limit = 5;
 let page = 1;
 
 // Fetch posts from API
 async function getPosts() {
   const res = await fetch(
-    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&page=${page}`
+    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
   );
 
   const data = await res.json();
@@ -31,9 +31,50 @@ async function showPosts() {
       </div>
     `;
 
-    postContainer.appendChild(postEl);
+    postsContainer.appendChild(postEl);
+  });
+}
+
+// Show loader & fetch more posts
+function showLoading() {
+  loading.classList.add("show");
+
+  setTimeout(() => {
+    loading.classList.remove("show");
+
+    setTimeout(() => {
+      page++;
+      showPosts();
+    }, 300);
+  }, 1000);
+}
+
+// Filter posts by input
+function filterPosts(e) {
+  const term = e.target.value.toUpperCase();
+  const posts = document.querySelectorAll(".post");
+
+  posts.forEach((post) => {
+    const title = post.querySelector(".post-title").innerText.toUpperCase();
+    const body = post.querySelector(".post-body").innerText.toUpperCase();
+
+    if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+      post.style.display = "flex";
+    } else {
+      post.style.display = "none";
+    }
   });
 }
 
 // Show initial posts
 showPosts();
+
+window.addEventListener("scroll", () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    showLoading();
+  }
+});
+
+filter.addEventListener("input", filterPosts);
